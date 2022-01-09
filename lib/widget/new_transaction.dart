f150c75f43c4a14ac876e9dc0ec310330d8da8b3
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-09 21:14:46 +0700

    memberikan ui friendly pada form input dengan media query

--- lib/widget/new_transaction.dart
+++ lib/widget/new_transaction.dart
@@ -50,56 +50,63 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 5,
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: _titleController,
-                onSubmitted: (_) => _submitData(),
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
-                controller: _amountCOntroller,
-                keyboardType: TextInputType.number,
-                onSubmitted: (_) => _submitData(),
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        _selectedDate == null
-                            ? 'No Date Chosen'
-                            : 'Picked Date : ${DateFormat.yMd().format(_selectedDate!)}',
-                      ),
-                    ),
-                    FlatButton(
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Card(
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.only(
+              top: 10,
+              right: 10,
+              left: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: _titleController,
+                  onSubmitted: (_) => _submitData(),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountCOntroller,
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (_) => _submitData(),
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          _selectedDate == null
+                              ? 'No Date Chosen'
+                              : 'Picked Date : ${DateFormat.yMd().format(_selectedDate!)}',
                         ),
                       ),
-                      onPressed: _presentDatePicker,
-                      textColor: Theme.of(context).primaryColor,
-                    )
-                  ],
+                      FlatButton(
+                        child: Text(
+                          'Choose Date',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: _presentDatePicker,
+                        textColor: Theme.of(context).primaryColor,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              RaisedButton(
-                onPressed: _submitData,
-                child: Text('Add Transactions'),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).textTheme.button!.color,
-              ),
-            ],
-          ),
-        ));
+                RaisedButton(
+                  onPressed: _submitData,
+                  child: Text('Add Transactions'),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button!.color,
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
